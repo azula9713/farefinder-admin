b168855037ee15229879196a5122ee0ff3b9b1b2
azula9173 <nnviduranga@gmail.com>
2022-02-19 01:58:17 +0530

    API Fix

--- src/api/Axios.js
+++ src/api/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: "http://localhost:1337/api/v1",
+  baseURL: "https://farefinderapi.devserver.click/api/v1",
 });
 
 export default Api;
